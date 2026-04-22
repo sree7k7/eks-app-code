440f5a7eaf2312b91ada3802471dc1bb3cd3e96a
Srikanth <sree7k7@gmail.com>
2026-04-22 15:52:05 +0200

    updated xd message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return jsonify({"status": "success", "message": "Platform Application yz Running!"})
+    return jsonify({"status": "success", "message": "Platform Application xd Running!"})
 
 @app.route("/health")
 def health():
